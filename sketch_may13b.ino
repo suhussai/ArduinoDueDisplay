24a5b3aeba5c81150aab663353dce057c31271b8
suhussai <suhussai@ualberta.ca>
2015-05-16 18:59:38 -0600

    Temperature added. Depth issue fixed, it now subscribes and displays published values depending on the topic it subscribes to

--- sketch_may13b.ino
+++ sketch_may13b.ino
@@ -1,13 +1,15 @@
-/*
+  /*
  * rosserial Subscriber Example
  * Blinks an LED on callback
  */
 
 #define USE_USBCON
+#include <string>
 #include "ros.h"
 #include "std_msgs/Empty.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
 #include "arvp_main/Depth.h"
 #include "arvp_main/Touch.h"
 #include "arvp_main/IMU.h"
@@ -50,24 +52,18 @@ data currentRoll;
 data currentPitch;
 data currentTouch;
 data currentDepth;
+data currentTemp;
 data all_values[5];
 
 
-void depthMessage( const arvp_main::Depth& DepthTopic){
-  nh.loginfo("displaying stuff");
-      myGLCD.setColor(255, 255, 255);
-  myGLCD.print("D: ", 0, 0);
-  myGLCD.printNumF(DepthTopic.depth, 2, 50, 0);
-//  delay(900);
-  
-}
 
 void setupData() {
  currentHeading.title = "H: ";
 currentRoll.title = "R: ";
 currentPitch.title = "P: ";
-currentTouch.title = "T: ";
+currentTouch.title = "To: ";
 currentDepth.title = "D: ";
+currentTemp.title = "Te: ";
 
 
 currentHeading.value = 0.0;
@@ -75,14 +71,15 @@ currentRoll.value = 0.0;
 currentPitch.value = 0.0;
 currentTouch.value = 0.0;
 currentDepth.value = 0.0;
+currentTemp.value = 0.0;
 
 
-
-currentHeading.title_x = 0;
-currentHeading.title_y = 60;
 
 currentDepth.title_x = 0;
 currentDepth.title_y = 0;
+
+currentHeading.title_x = 0;
+currentHeading.title_y = 60;
 
 currentRoll.title_x = 0;
 currentRoll.title_y = 120;
@@ -90,8 +87,11 @@ currentRoll.title_y = 120;
 currentPitch.title_x = 0;
 currentPitch.title_y = 180;
 
-currentTouch.title_x = 150;
+currentTouch.title_x = 140;
 currentTouch.title_y = 0;
+
+currentTemp.title_x = 140;
+currentTemp.title_y = 60;
 
 
 
@@ -109,15 +109,18 @@ currentRoll.data_y = 120;
 currentPitch.data_x = 50;
 currentPitch.data_y = 180;
 
-currentTouch.data_x = 200;
+currentTouch.data_x = 190;
 currentTouch.data_y = 0;
 
+currentTemp.data_x = 190;
+currentTemp.data_y = 60;
 
 currentHeading.decimal_places = 2;
 currentDepth.decimal_places = 2;
 currentRoll.decimal_places = 2;
 currentPitch.decimal_places = 2;
 currentTouch.decimal_places = 2;
+currentTemp.decimal_places = 2;
 
 data all_values[5] = {currentHeading, currentRoll, currentPitch, currentTouch, currentDepth};
 
@@ -127,7 +130,7 @@ data all_values[5] = {currentHeading, currentRoll, currentPitch, currentTouch, c
 void updateAllValuesWithCurrentValues() {
     // print (decimal places, x ,y )
   
-    myGLCD.clrScr();
+    myGLCD.clrScr();  
     
     /*
     
@@ -150,20 +153,25 @@ void updateAllValuesWithCurrentValues() {
     myGLCD.print(currentDepth.title, currentDepth.title_x, currentDepth.title_y);
     myGLCD.printNumF(currentDepth.value, currentDepth.decimal_places, currentDepth.data_x, currentDepth.data_y);
 
-    
-    myGLCD.print("H: ", 0, 60);
-    myGLCD.printNumF(currentHeading.value, 2, 50, 60);
+ 
+    myGLCD.print(currentHeading.title, currentHeading.title_x, currentHeading.title_y);
+    myGLCD.printNumF(currentHeading.value, currentHeading.decimal_places, currentHeading.data_x, currentHeading.data_y);
 
 
-    myGLCD.print("R: ", 0, 120);
-    myGLCD.printNumF(currentRoll.value, 2, 50, 120);
+    myGLCD.print(currentRoll.title, currentRoll.title_x, currentRoll.title_y);
+    myGLCD.printNumF(currentRoll.value, currentRoll.decimal_places, currentRoll.data_x, currentRoll.data_y);
 
 
-    myGLCD.print("P: ", 0, 180);
-    myGLCD.printNumF(currentPitch.value, 2, 50, 180);
+    myGLCD.print(currentPitch.title, currentPitch.title_x, currentPitch.title_y);
+    myGLCD.printNumF(currentPitch.value, currentPitch.decimal_places, currentPitch.data_x, currentPitch.data_y);
 
-    myGLCD.print("T: ", 200, 0);
-    myGLCD.printNumF(currentTouch.value, 2, 250, 0);
+
+    myGLCD.print(currentTouch.title, currentTouch.title_x, currentTouch.title_y);
+    myGLCD.printNumF(currentTouch.value, currentTouch.decimal_places, currentTouch.data_x, currentTouch.data_y);
+
+
+    myGLCD.print(currentTemp.title, currentTemp.title_x, currentTemp.title_y);
+    myGLCD.printNumF(currentTemp.value, currentTemp.decimal_places, currentTemp.data_x, currentTemp.data_y);
 
 }
 
@@ -179,7 +187,7 @@ void IMUMessage( const arvp_main::IMU& IMUTopic){
      
    }
    
-   else if (IMUTopic.pitch != currentPitch.value) {
+   if (IMUTopic.pitch != currentPitch.value) {
     currentPitch.value = IMUTopic.pitch;
       updateAllValuesWithCurrentValues();
 //    myGLCD.setColor(255, 255, 255);
@@ -188,7 +196,7 @@ void IMUMessage( const arvp_main::IMU& IMUTopic){
      
    }
 
-   else if (IMUTopic.roll != currentRoll.value) {
+   if (IMUTopic.roll != currentRoll.value) {
     currentRoll.value = IMUTopic.roll;
       updateAllValuesWithCurrentValues();
 //    myGLCD.setColor(255, 255, 255);
@@ -196,32 +204,24 @@ void IMUMessage( const arvp_main::IMU& IMUTopic){
 //    myGLCD.printNumF(currentRoll.value, currentRoll.decimal_places, currentRoll.data_x, currentRoll.data_y);
      
    }
- 
+   
 //  delay(900);
   
   
 }
 
 
-void RollMessage( const std_msgs::String& Roll){
-//  nh.loginfo(Depth.data);
-  char * message;
-  sprintf(message, "Roll: %s",Roll.data);
-  myGLCD.print(message, LEFT, 200);
-//  delay(900);
+
+void depthMessage( const arvp_main::Depth& DepthTopic){
+  if(DepthTopic.depth != currentDepth.value) {
+    currentDepth.value = DepthTopic.depth;
+    updateAllValuesWithCurrentValues();
+     
+ }  
   
 }
 
 
-void pitchMessage( const std_msgs::String& Pitch){
-//  nh.loginfo(Depth.data);
-  char * message;
-  sprintf(message, "Pitch: %s",Pitch.data);
-  myGLCD.print(message, CENTER, 1);
-//  delay(900);
-  
-  
-}
 
 void PIDMessage( const std_msgs::String& PID){
 //  nh.loginfo(Depth.data);
@@ -234,27 +234,25 @@ void PIDMessage( const std_msgs::String& PID){
 }
 
 
+void tempMessage( const std_msgs::Float32& TempTopic){
+  if(TempTopic.data != currentTemp.value) {
+      currentTemp.value = TempTopic.data;
+      updateAllValuesWithCurrentValues();
+     
+   }  
+}
+
+
 void touchMessage( const arvp_main::Touch& TouchTopic){
   if(TouchTopic.touch != currentTouch.value) {
-    currentTouch.value = TouchTopic.touch;
+      currentTouch.value = TouchTopic.touch;
       updateAllValuesWithCurrentValues();
-    
-//    myGLCD.setColor(255, 255, 255);
-//    myGLCD.print(currentTouch.title, currentTouch.title_x , currentTouch.title_y);
-//    myGLCD.printNumF(currentTouch.value, currentTouch.decimal_places, currentTouch.data_x, currentTouch.data_y);
      
    }  
 }
 
 
 
-void errorMessage( const std_msgs::String& ErrorTopic) {
-//  nh.loginfo(Depth.data);
-  displayErrorLines("Error Reported!!!", ErrorTopic.data, "");
-  delay(1000);
-  
-  
-}
 
 
 
@@ -269,35 +267,50 @@ void batteryMessage( const std_msgs::String& Battery) {
 }
 
 
-void displayErrorLines(String line1, String line2, String line3) {
-
+void displayErrorLines(String line) {
+      
       myGLCD.clrScr();
       myGLCD.setFont(SmallFont);
       myGLCD.fillScr(0, 0, 255);
       myGLCD.setColor(255, 0, 0);
-      myGLCD.fillRoundRect(80, 70, 239, 169);
+      myGLCD.fillRoundRect(0, 70, 319, 169);
     
       myGLCD.setColor(255, 255, 255);
       myGLCD.setBackColor(255, 0, 0);
-      myGLCD.print(line1, CENTER, 93);
-      myGLCD.print(line2, CENTER, 119);
+      
+//      int starting = 93;
+//      while(line.length() > 24) {
+//        myGLCD.print(line.substr(0,24).c_str(), CENTER, starting);
+//        line = line.substr(24);       
+//        starting = starting + 26;        
+//      }
+      
+      myGLCD.print(line.c_str(), CENTER, 93);
+
+/*      myGLCD.print(line2, CENTER, 119);
       myGLCD.print(line3, CENTER, 132);
+    */  
+}
+
+
+void errorMessage( const std_msgs::String& ErrorTopic) {
+//  nh.loginfo(Depth.data);
+  displayErrorLines(ErrorTopic.data);
+  delay(1000);
+  
   
 }
 
 
-
-
-ros::Subscriber<arvp_main::Depth> depthSub("sensors/Depth", &depthMessage );
 ros::Subscriber<arvp_main::IMU> IMUSub("sensors/imu", &IMUMessage );
 
 ros::Subscriber<arvp_main::Touch> touchSub("sensors/touch", &touchMessage );
+ros::Subscriber<std_msgs::Float32> tempSub("sensors/temp", &tempMessage );
 
 ros::Subscriber<std_msgs::String> errorSub("display/errors", &errorMessage );
+ros::Subscriber<arvp_main::Depth> depthSub("sensors/depth", &depthMessage );
 
 
-ros::Subscriber<std_msgs::String> RollSub("Roll", &RollMessage );
-ros::Subscriber<std_msgs::String> pitchSub("Pitch", &pitchMessage );
 ros::Subscriber<std_msgs::String> PIDSub("PID", &PIDMessage );
 ros::Subscriber<std_msgs::String> batterySub("Battery", &batteryMessage );
 
@@ -311,6 +324,7 @@ void setup()
   // Setup the LCD
   myGLCD.InitLCD();
   myGLCD.setFont(BigFont);
+  myGLCD.setBrightness(16);
 
 
   // subscribe to required topics  
@@ -318,6 +332,7 @@ void setup()
   nh.subscribe(depthSub);
   nh.subscribe(IMUSub);
   nh.subscribe(touchSub);
+  nh.subscribe(tempSub);
   nh.subscribe(errorSub);
   //  nh.subscribe(headingSub);
  // nh.subscribe(RollSub);
@@ -352,7 +367,7 @@ void loop()
   else if (mainDataPage && !nh.connected()) {
 
     
-      displayErrorLines("We Aren't Connected", "Checking connection", "in a few seconds...");
+      displayErrorLines("We Aren't Connected \n Checking connection in a few seconds...");
       
       
       delay(300); 
